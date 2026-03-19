ad8cf8c2a18cb61a4b9f3180d5273e1a7879c99c
StrangeDuckk <mikusek.kasia@gmail.com>
2026-03-19 15:21:41 +0100

    Updated requirements

--- MP1/src/Main.java
+++ MP1/src/Main.java
@@ -7,35 +7,54 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("===== Poczatek MP1 MAS =====\n");
 
-        //todo pare przykladowych obiektow i dzialania na nich
-        //todo zapis do pliku BINARNEGO na koniec i wczytanie na poczatku wszystkich informacji przez funkcje klasowe
+        // ------- tworzenie obiektow -------
+        
 
 
-        //zapis do pliku w jednym miejscu, nie w konstrukturach, tylko jak sie wszystko utworzy to wtedy
-        //bez zadnego menu w konsoli
-        //sprawdzenie czy plik binarny juz istnieje czy nie, jesli istnieje to wtedy jest odczyt
+        // ------- dzialanie na obiektach --------
 
-        //wysylanie projektu w zip na gakko, dodac diagram uml
+        // ------- zapis do pliku binarnego -------
 
-
-
-        //todo zapis do pliku bin w main inputstreama przekazujemy do kazdej klasy tak jak na wykladzie
-        //wyklad 4 implementacja serializable (35/50) (jako zapisujacy stream lecacy po kazdej klasie)
-        //w atrybucie zlozonym nie mozna zrobic obiektu w ktorym bedzie asocjacja, w klasie atrybutu zlozonego uzywac tylko prymitywow (mam)
-        //localdate jest zlozonym
     }
 }
+//todo w setterach dla wszystkich typow poza prostymi sprawdzenie czy wartosc nie jest null (5.2)
+
+//todo pare przykladowych obiektow i dzialania na nich
+//todo zapis do pliku BINARNEGO na koniec i wczytanie na poczatku wszystkich informacji przez funkcje klasowe
+//todo implementacja Serializable
+//todo statyczne metody zapisu i odczytu ekstensji z pliku, jedna para metod prowadzacych zapis i odczyt na wszystkich ekstensjach do jednego pliku
+//todo try with resources
 
 
-//todo wymagania:
-// ekstensja klasy
-// ekstensja zabezpieczenie i trwalosc
-// atrybut zlozony
-// atrybut opcjonalny
-// atrybut powtarzalny
+
+//zapis do pliku w jednym miejscu, nie w konstrukturach, tylko jak sie wszystko utworzy to wtedy
+//bez zadnego menu w konsoli
+//sprawdzenie czy plik binarny juz istnieje czy nie, jesli istnieje to wtedy jest odczyt
+
+//wysylanie projektu w zip na gakko, dodac diagram uml
+
+
+
+//todo zapis do pliku bin w main inputstreama przekazujemy do kazdej klasy tak jak na wykladzie
+//wyklad 4 implementacja serializable (35/50) (jako zapisujacy stream lecacy po kazdej klasie)
+//w atrybucie zlozonym nie mozna zrobic obiektu w ktorym bedzie asocjacja, w klasie atrybutu zlozonego uzywac tylko prymitywow (mam)
+//localdate jest zlozonym
+
+//todo wymagania
+
+// todo ekstensja klasy
+// todo ekstensja zabezpieczenie i trwalosc
+// atrybut zlozony (jest)
+// atrybut opcjonalny (jest ale dopytac czy ok)
+// todo atrybut wymagany
+// todo atrybut powtarzalny -> 6.3 (dodawanie (+walidacja) i usuwanie wartosci z atrybutu (waruenk ze jesli nie opcjonalny to nie moze tablica byc pusta), zabezpieczenie jak w ekstensji)
 // atrybut klasowy
 // atrybut pochodny
 // metoda klasowa
-// przeciazenie, przesloniecie +
+// przeciazenie, przesloniecie 
 
 //todo zapis wszystkiego z ekstensji do pliku bin
+
+//todo zapytac:
+//czy w tych polach gdzie argumenty sa Nie opcjonalne sprawdzac czy wartosc argumentu to null czy nie?
+//czy rozwiazanie takie jak w Adress jest ok? zamiast null jest 0
