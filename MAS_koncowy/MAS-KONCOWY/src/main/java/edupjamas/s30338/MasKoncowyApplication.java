ddd45d6ec3ff529128c1a27a2f92592838e17491
StrangeDuckk <mikusek.kasia@gmail.com>
2026-06-02 00:14:01 +0200

    initial project

---
+++ MAS_koncowy/MAS-KONCOWY/src/main/java/edupjamas/s30338/MasKoncowyApplication.java
@@ -0,0 +1,26 @@
+package edupjamas.s30338;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasKoncowyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MasKoncowyApplication.class, args);
+
+    }
+}
+
+
+/*
+todo
+zmienic na koniec z ustawien w application.properties create-drop na cos innego zeby miec dane
+
+todo klasy- encje -> pola + ID
+todo relacje
+
+todo hibernate
+todo postgres
+todo repozytoria i serwisy
+todo FX
+ */
---
+++ MAS_koncowy/MAS-KONCOWY/src/test/java/edupjamas/s30338/MasKoncowyApplicationTests.java
@@ -0,0 +1,13 @@
+package edupjamas.s30338;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasKoncowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
